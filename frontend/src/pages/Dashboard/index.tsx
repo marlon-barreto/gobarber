24f621966e7f48238c233e2691acd91868d8e63c
Marlon Nery <marlonery@msn.com>
2020-06-15 23:52:06 -0300

    Dashboard implementacao

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
+import { Container, Hearder, HearderContent, Profile } from './styles';
 
-const Dashboard: React.FC = () => <h1>Dasborad</h1>;
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
+
+const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
+  return (
+    <Container>
+      <Hearder>
+        <HearderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Profile>
+            <img src={user.avatar_url} alt={user.name} />
+
+            <div>
+              <span>Bem-Vindo,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HearderContent>
+      </Hearder>
+    </Container>
+  );
+};
 
 export default Dashboard;
