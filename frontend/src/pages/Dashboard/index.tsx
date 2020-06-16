a2153c8acea0ba3fc8a5b56bec3e9f9dc162a029
Marlon Nery <marlonery@msn.com>
2020-06-16 00:53:23 -0300

    Dashboard

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import { FiPower } from 'react-icons/fi';
-import { Container, Hearder, HearderContent, Profile } from './styles';
+import React, { useState } from 'react';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Hearder,
+  HearderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointmet,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectDate, setSelectDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -28,6 +41,73 @@ const Dashboard: React.FC = () => {
           </button>
         </HearderContent>
       </Hearder>
+
+      <Content>
+        <Schedule>
+          <h1>Horaios agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img src={user.avatar_url} alt={user.name} />
+
+              <strong>{user.name}</strong>
+              <span>
+                <FiClock /> 08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manha</strong>
+
+            <Appointmet>
+              <span>
+                <FiClock /> 08:00
+              </span>
+
+              <div>
+                <img src={user.avatar_url} alt={user.name} />
+
+                <strong>{user.name}</strong>
+              </div>
+            </Appointmet>
+
+            <Appointmet>
+              <span>
+                <FiClock /> 09:00
+              </span>
+
+              <div>
+                <img src={user.avatar_url} alt={user.name} />
+
+                <strong>{user.name}</strong>
+              </div>
+            </Appointmet>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointmet>
+              <span>
+                <FiClock /> 14:00
+              </span>
+
+              <div>
+                <img src={user.avatar_url} alt={user.name} />
+
+                <strong>{user.name}</strong>
+              </div>
+            </Appointmet>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
